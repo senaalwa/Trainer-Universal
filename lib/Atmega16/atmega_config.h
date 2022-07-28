d42ff3d861724b5a2003cf5e9a71f27c3f5d0f49
senaalwa <fatur.sena07@gmail.com>
2022-07-28 10:37:52 +0700

    deleted_file_28Juli22

--- lib/Atmega16/atmega_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ATMEGA_CONFIG_H_
-#define _ATMEGA_CONFIG_H_
-
-#ifndef __AVR_ATmega16__
-    #define __AVR_ATmega16__
-#endif
-
-#endif
--- lib/ov7670/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LIB_CONFIG_
-#define _LIB_CONFIG_
-
-#define _PLATFORM_ATMEGA16
-
-#endif
